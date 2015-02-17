46d3261192dabba6f65eb8a8dcc91764c6a063aa
johnnyvf24 <johnnyvf24@gmail.com>
2015-02-16 22:45:35 -0500

    Added_Rendering_map_scrolling_and_locking

--- test/core/src/com/postapocgame/game/MyPostApocGame.java
+++ test/core/src/com/postapocgame/game/MyPostApocGame.java
@@ -1,13 +1,13 @@
 package com.postapocgame.game;
 
 import com.badlogic.gdx.Game;
-import com.postapocgame.game.screens.IntroScreen;
+import com.postapocgame.game.screens.WorldMap;
 
 public class MyPostApocGame extends Game {
 
 	@Override
 	public void create() {
-		setScreen(new IntroScreen(this));
+		setScreen(new WorldMap(this));
 		
 	}
 
--- test/desktop/src/com/postapocgame/game/desktop/DesktopLauncher.java
+++ test/desktop/src/com/postapocgame/game/desktop/DesktopLauncher.java
@@ -8,6 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new MyPostApocGame(), config);
-		config.resizable = false;
+		config.width = 1280;
+		config.height = 680;
 	}
 }
